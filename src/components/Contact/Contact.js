5ba424afc58d39dc4a21eda94f48263ce3ac3e10
Steven Granados <stevengranados0211@gmail.com>
2020-04-15 14:50:47 -0600

    Contact component structure added, information pending to be added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 export default class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Contact!</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>Half page</Cell>
+          <Cell col={6}> Half page</Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
